22a086988df8ad9160cd5e5ba90d42042222bfe9
Vinodhr <vinodhrodrigues@gmail.com>
2013-11-02 16:48:59 -0400

    Edited project.v
    Edited project.v file to be able to read U and V values into registers
    with appropriate spacing and to write those values into the SRAM with
    appropriate spacing. The U and V segments now cover all the unused
    memory spaces.

--- M1/project.v
+++ M1/project.v
@@ -61,6 +61,7 @@ logic [3:0] PB_pushed;
 logic [9:0] VGA_red, VGA_green, VGA_blue;
 logic [9:0] pixel_X_pos;
 logic [9:0] pixel_Y_pos;
+logic [15:0] count;
 
 logic resetn;
 
@@ -70,12 +71,14 @@ logic [15:0] SRAM_write_data;
 logic SRAM_we_n;
 logic [15:0] SRAM_read_data;
 logic SRAM_ready;
+//
+//logic [2:0] rect_row_count;		// Number of rectangles in a row
+//logic [2:0] rect_col_count;		// Number of rectangles in a column
+//logic [5:0] rect_width_count;	// Width of each rectangle
+//logic [4:0] rect_height_count;	// Height of each rectangle
+//logic [2:0] color;
+logic [15:0][108543] uv_reg;
 
-logic [2:0] rect_row_count;		// Number of rectangles in a row
-logic [2:0] rect_col_count;		// Number of rectangles in a column
-logic [5:0] rect_width_count;	// Width of each rectangle
-logic [4:0] rect_height_count;	// Height of each rectangle
-logic [2:0] color;
 
 logic [15:0] VGA_sram_data [2:0];
 
@@ -133,194 +136,126 @@ VGA_Controller VGA_unit(
 
 // Each rectangle will have different color
 assign color = rect_col_count + rect_row_count;
+enum logic [2:0] {
+	S_IDLE,
+	S_DELAY_1,
+	S_DELAY_2,
+	S_WRITE_CYCLE,
+	S_READ_CYCLE,
+	S_DELAY_3,
+	S_DELAY_4
+} BIST_state;
 
 always_ff @ (posedge CLOCK_50_I or negedge resetn) begin
 	if (resetn == 1'b0) begin
 		state <= S_IDLE;
-		rect_row_count <= 3'd0;
-		rect_col_count <= 3'd0;
-		rect_width_count <= 6'd0;
-		rect_height_count <= 5'd0;
-		
-		VGA_red <= 10'd0;
-		VGA_green <= 10'd0;
-		VGA_blue <= 10'd0;				
+//		rect_row_count <= 3'd0;
+//		rect_col_count <= 3'd0;
+//		rect_width_count <= 6'd0;
+//		rect_height_count <= 5'd0;
+//		
+//		VGA_red <= 10'd0;
+//		VGA_green <= 10'd0;
+//		VGA_blue <= 10'd0;				
 		
 		SRAM_we_n <= 1'b1;
 		SRAM_write_data <= 16'd0;
-		SRAM_address <= 18'd0;
-	end else begin
-		case (state)
-		S_IDLE: begin
-			if (PB_pushed[0] == 1'b1) begin
-				// Start filling the SRAM
-				state <= S_FILL_SRAM_0;
-				SRAM_address <= 18'h3FFFF;				
-			end
-		end
-		S_FILL_SRAM_0: begin
-			SRAM_we_n <= 1'b0;
-			// Fill Red, Green data for first pixel
-			SRAM_write_data <= {{8{color[2]}}, {8{color[1]}}};
-			SRAM_address <= SRAM_address + 18'd1;
-			
-			state <= S_FILL_SRAM_1;			
-		end
-		S_FILL_SRAM_1: begin
-			// Fill Blue data for first pixel and Red data for second pixel
-			SRAM_write_data <= {{8{color[0]}}, {8{color[2]}}};
-			SRAM_address <= SRAM_address + 18'd1;
-
-			rect_width_count <= rect_width_count + 6'd1;
-
-			state <= S_FILL_SRAM_2;
-		end
-		S_FILL_SRAM_2: begin
-			// Fill Green, Blue data for second pixel
-			SRAM_write_data <= {{8{color[1]}}, {8{color[0]}}};
-			SRAM_address <= SRAM_address + 18'd1;
-							
-			if ((rect_row_count == (NUM_ROW_RECTANGLE - 1))
-			 && (rect_col_count == (NUM_COL_RECTANGLE - 1))
-			 && (rect_width_count == (RECT_WIDTH - 1))
-			 && (rect_height_count == (RECT_HEIGHT - 1))) begin
-				// Finish filling the SRAM
-				state <= S_FINISH_FILL_SRAM;										
-			end else begin					
-				if (rect_width_count < 'd39) begin
-					// Same horizontal line for a rectangle
-					rect_width_count <= rect_width_count + 6'd1;
-				end else begin
-					// Move to the next rectangle
-					rect_width_count <= 6'd0;
-					if (rect_col_count < (NUM_COL_RECTANGLE - 1)) rect_col_count <= rect_col_count + 3'd1;
-					else begin
-						// One pixel row
-						if (rect_height_count < (RECT_HEIGHT - 1)) begin
-							rect_height_count <= rect_height_count + 5'd1;
-						end else begin
-							// New row of rectangle
-							rect_row_count <= rect_row_count + 3'd1;
-							rect_height_count <= 5'd0;																
-						end
-						rect_col_count <= 3'd0;						
-					end
-				end
-				state <= S_FILL_SRAM_0;
-			end
-		end
-		S_FINISH_FILL_SRAM: begin
-			// Finish filling the SRAM
-			// Now prepare for VGA to read the image
-			SRAM_we_n <= 1'b1;
-			
-			VGA_sram_data[2] <= 16'd0;
-			VGA_sram_data[1] <= 16'd0;
-			VGA_sram_data[0] <= 16'd0;
-
-			SRAM_address <= 18'h00000;									
-			
-			state <= S_WAIT_NEW_PIXEL_ROW;			
-		end
-		S_WAIT_NEW_PIXEL_ROW: begin
-			if (pixel_Y_pos >= VIEW_AREA_TOP && pixel_Y_pos < VIEW_AREA_BOTTOM) begin
-				if (pixel_X_pos == (VIEW_AREA_LEFT - 3)) begin
-					if (pixel_Y_pos == VIEW_AREA_TOP) 
-						// Start a new frame
-						// Provide address for data 1
-						SRAM_address <= 18'd0;
-					else 
-						// Start a new row of pixels
-						// Provide address for data 1
-						SRAM_address <= SRAM_address - 18'd4;
-					
-					state <= S_NEW_PIXEL_ROW_DELAY_1;
-				end
-			end
-			
-			VGA_red <= 10'd0;
-			VGA_green <= 10'd0;
-			VGA_blue <= 10'd0;								
-		end
-		S_NEW_PIXEL_ROW_DELAY_1: begin	
-			// Provide address for data 2
-			SRAM_address <= SRAM_address + 18'h00001;				
-			state <= S_NEW_PIXEL_ROW_DELAY_2;		
-		end
-		S_NEW_PIXEL_ROW_DELAY_2: begin	
-			// Provide address for data 3
-			SRAM_address <= SRAM_address + 18'h00001;		
-			state <= S_NEW_PIXEL_ROW_DELAY_3;		
-		end
-		S_NEW_PIXEL_ROW_DELAY_3: begin		
-			// Buffer data 1
-			VGA_sram_data[2] <= SRAM_read_data;			
-					
-			state <= S_NEW_PIXEL_ROW_DELAY_4;
-		end
-		S_NEW_PIXEL_ROW_DELAY_4: begin
-			// Provide address for data 1
-			SRAM_address <= SRAM_address + 18'h00001;
-			
-			// Buffer data 2
-			VGA_sram_data[1] <= SRAM_read_data;			
-			
-			state <= S_NEW_PIXEL_ROW_DELAY_5;			
-		end
-		S_NEW_PIXEL_ROW_DELAY_5: begin
-			// Provide address for data 2
-			SRAM_address <= SRAM_address + 18'h00001;
+		SRAM_address <= 18'h9600;
 		
-			// Buffer data 3
-			VGA_sram_data[0] <= SRAM_read_data;
-					
-			state <= S_FETCH_PIXEL_DATA_0;			
+	end else begin
+			case (SRAM_state)
+		S_IDLE: begin
+		////////////////////////////////////////
+		/////                             //////
+		/////  FIX THE START CONDITION    //////
+		////////////////////////////////////////
+			if (BIST_start & ~BIST_start_buf) begin
+				// Start the BIST engine
+				SRAM_address <= 18'9600;
+				SRAM_we_n <= 1'b1;
+				SRAM_write_data <= 16'd0;
+				//BIST_mismatch <= 1'b0;
+				
+				SRAM_finish <= 1'b0;
+				count <= 15'b0;
+				
+				SRAM_state <= S_DELAY_1;
+			end else begin
+				SRAM_address <= 18'd0;
+				SRAM_write_data <= 16'd0;
+				SRAM_we_n <= 1'b1;
+				SRAM_finish <= 1'b1;				
+			end
 		end
-		S_FETCH_PIXEL_DATA_0: begin
-			// Provide address for data 3
-			SRAM_address <= SRAM_address + 18'h00001;
-					
-			// Provide RGB data
-			VGA_red <= {VGA_sram_data[2][15:8], 2'b00};
-			VGA_green <= {VGA_sram_data[2][7:0], 2'b00};
-			VGA_blue <= {VGA_sram_data[1][15:8], 2'b00};			
-			
-			state <= S_FETCH_PIXEL_DATA_1;			
+		S_DELAY_1: begin
+			// Delay for reading values starting at U into register
+			SRAM_address <= SRAM_address + 18'd1;
+			SRAM_state <= S_DELAY_2;
 		end
-		S_FETCH_PIXEL_DATA_1: begin		
-			// Buffer data 1
-			VGA_sram_data[2] <= SRAM_read_data;
-					
-			state <= S_FETCH_PIXEL_DATA_2;
+		S_DELAY_2: begin
+			// Delay for reading values starting at U into register
+			BIST_address <= SRAM_address + 18'd1;
+			BIST_state <= S_READ_CYCLE;
 		end
-		S_FETCH_PIXEL_DATA_2: begin				
-			// Provide address for data 1
-			SRAM_address <= SRAM_address + 18'h00001;
+		S_WRITE_CYCLE: begin
+			// SRAM is writing in this clock cycle
+			if(SRAM_address>=18'h23DFF)begin
+				SRAM_we_n <= 1'b1;
+				SRAM_address <= SRAM_address + 18'd1;	
+				
+				SRAM_state <= S_DELAY_3;
 			
-			// Buffer data 2
-			VGA_sram_data[1] <= SRAM_read_data;
 			
-			// Provide RGB data
-			VGA_red <= {VGA_sram_data[1][7:0], 2'b00};
-			VGA_green <= {VGA_sram_data[0][15:8], 2'b00};
-			VGA_blue <= {VGA_sram_data[0][7:0], 2'b00};
+			end
+			else begin
+				
+				SRAM_we_n <= 1'b0;
+				SRAM_write_data <= uv_reg[count];
+				SRAM_address <= SRAM_address + 18'd1;
+				count <= count +18'd1;
+				SRAM_state <= S_WRITE_CYCLE;			
 			
-			state <= S_FETCH_PIXEL_DATA_3;
+			end
 		end
-		S_FETCH_PIXEL_DATA_3: begin 
-			// Provide address for data 2
-			SRAM_address <= SRAM_address + 18'h00001;			
-			
-			// Buffer data 3
-			VGA_sram_data[0] <= SRAM_read_data;
+		S_READ_CYCLE: begin
+			// SRAM is reading in this clock cycle
+			// Check for data mismatch
+		//	if (BIST_read_data != (BIST_address[15:0]-16'd2)) BIST_mismatch <= 1'b1;
+		if (SRAM_address < 18'h12BFF) begin
+				// Increment address and continue
+				SRAM_address <= SRAM_address + 18'd1;
+				uv_reg[count] <= SRAM_Read_data;
+				SRAM_state <= S_READ_CYCLE;
+				count <= count+ 15d'2;
+			end else begin
+				// Delay for loading the last 2 addresses into register
+				SRAM_state <= S_DELAY_3;
+				uv_reg[count] <= SRAM_Read_data;
+				count <= count+ 15d'2
 						
-			if (pixel_X_pos < (VIEW_AREA_RIGHT - 2))
-				// Still within one row
-				state <= S_FETCH_PIXEL_DATA_0;
-			else
-				state <= S_WAIT_NEW_PIXEL_ROW;
-		end		
-		default: state <= S_IDLE;
+			end
+		end
+		S_DELAY_3: begin
+			SRAM_state <= S_DELAY_4;
+			uv_reg[count] <= SRAM_Read_data;
+			count <= count+ 15d'2;
+//			BIST_state <= S_IDLE;
+//			if (BIST_read_data != (BIST_address[15:0]-16'd2)) BIST_mismatch <= 1'b1;
+		end
+		S_DELAY_4: begin
+			SRAM_state <= S_WRITE_CYCLE;
+			uv_reg[count] <= SRAM_Read_data;
+			SRAM_address <= 18'h9600;
+			count <= 15'h0;
+			// Check for data mismatch
+//			if (BIST_read_data != (BIST_address[15:0]-16'd2)) BIST_mismatch <= 1'b1;
+//			
+//			// Finish the whole SRAM
+//			BIST_state <= S_IDLE;
+//							
+//			BIST_finish <= 1'b1;	
+		end
+		default: SRAM_state <= S_IDLE;
 		endcase
 	end
 end
