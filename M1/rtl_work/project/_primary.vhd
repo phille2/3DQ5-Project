4dd942a8818771ca696b3edd32222dad145cb889
phille2 <eric.phillipsheldon@gmail.com>
2013-11-15 19:43:20 -0500

    Modelsim Files

--- M1/rtl_work/project/_primary.vhd
+++ M1/rtl_work/project/_primary.vhd
@@ -1,15 +1,14 @@
 library verilog;
 use verilog.vl_types.all;
 entity project is
-    generic(
-        U_OFFSET        : vl_logic_vector(0 to 17) := (Hi0, Hi0, Hi1, Hi0, Hi0, Hi1, Hi0, Hi1, Hi1, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0);
-        V_OFFSET        : vl_logic_vector(0 to 17) := (Hi0, Hi0, Hi1, Hi1, Hi1, Hi0, Hi0, Hi0, Hi0, Hi1, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0);
-        RGB_OFFSET      : vl_logic_vector(0 to 17) := (Hi1, Hi0, Hi0, Hi0, Hi1, Hi1, Hi1, Hi1, Hi1, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0, Hi0)
-    );
     port(
         CLOCK_50_I      : in     vl_logic;
         PUSH_BUTTON_I   : in     vl_logic_vector(3 downto 0);
         SWITCH_I        : in     vl_logic_vector(17 downto 0);
+        milestone1_enable: out    vl_logic;
+        milestone1_done : out    vl_logic;
+        SEVEN_SEGMENT_N_O: out    vl_logic;
+        LED_GREEN_O     : out    vl_logic_vector(8 downto 0);
         VGA_CLOCK_O     : out    vl_logic;
         VGA_HSYNC_O     : out    vl_logic;
         VGA_VSYNC_O     : out    vl_logic;
@@ -24,10 +23,8 @@ entity project is
         SRAM_LB_N_O     : out    vl_logic;
         SRAM_WE_N_O     : out    vl_logic;
         SRAM_CE_N_O     : out    vl_logic;
-        SRAM_OE_N_O     : out    vl_logic
+        SRAM_OE_N_O     : out    vl_logic;
+        UART_RX_I       : in     vl_logic;
+        UART_TX_O       : out    vl_logic
     );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of U_OFFSET : constant is 1;
-    attribute mti_svvh_generic_type of V_OFFSET : constant is 1;
-    attribute mti_svvh_generic_type of RGB_OFFSET : constant is 1;
 end project;
